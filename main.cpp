9991d81bd8181b62563ec07d03bba4b232960687
kbfey_HP_win764bit <542633674@qq.com>
2018-04-23 15:05:37 +0800

    测试if语句

--- main.cpp
+++ main.cpp
@@ -8,10 +8,17 @@ int main()
     cout << "enter a int" << endl;
     int int1 = 0;
     cin >> int1;
-    cout << int1 << endl << "end" << endl;
+    if(int1 > 0){
+        cout << "大于0的整数" << endl;
+    }
+    else {
+        cout << "小于0的整数" << endl;
+    }
+    cout << int1 << endl;
     int arr1[5] = {1,2,3,4,5};
     for(int i = 0;i < 5;i++){
         cout << arr1[i] << endl;
     }
+    cout << "end" << endl;
     return 0;
 }
