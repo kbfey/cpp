92cbe0d4b494f58883cb30eef6201e4946ceeaa3
kbfey_HP_win764bit <542633674@qq.com>
2018-04-23 14:27:22 +0800

    测试cin

--- main.cpp
+++ main.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << "enter a int" << endl;
+    int int1 = 0;
+    cin >> int1;
+    cout << int1 << endl << "end" << endl;
     return 0;
 }
